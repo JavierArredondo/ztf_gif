03fe9593375e33700c0ccc7f3e3f07e2e885332f
JavierArredondo <javier.arredondo.c@usach.cl>
2022-08-07 16:43:44 -0400

    chore(setup): create setup file and some project configs

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from ztf_gif import __version__
+from setuptools import find_packages, setup
+
+with open("requirements.txt") as f:
+    required_packages = f.readlines()
+
+setup(
+    name="ztf_gif",
+    version=__version__,
+    description="",
+    author="JavierArredondo",
+    author_email="javier.arredondo.c@usach.cl",
+    include_package_data=True,
+    packages=find_packages(),
+    install_requires=required_packages,
+    build_requires=required_packages,
+    project_urls={
+        "Github": "https://github.com/JavierArredondo/ztf_gif",
+        "Documentation": "",
+    },
+)
--- ztf_gif/__init__.py
+++ ztf_gif/__init__.py
@@ -1,1 +1,3 @@
 from .stamps import make_gif
+
+__version__ = "1.0.0"
